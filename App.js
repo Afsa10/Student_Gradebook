93a9ce5cfc3e95e29c310a18cf0c660696f25c97
Afsa Banu <150768199+Afsa10@users.noreply.github.com>
2024-10-20 13:29:53 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Navigation from './project/Navigation';
+
+
+
+function App() {
+
+  return (
+   <div className="App">
+
+
+  <Navigation/>
+    </div>
+  );
+}
+
+export default App;
